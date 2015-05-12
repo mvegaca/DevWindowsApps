9791ae7e4cf0d92257b237b613c6dc40cb083703
Martin Vega <mvegaca@outlook.com>
2015-05-12 10:05:04 +0200

    Add XBind Poc

---
+++ PoC/XBind/XBind/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace XBind
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public string StringProp
+        {
+            get { return "Current Time:"; }
+        }
+        private string _timeProp = DateTime.Now.ToString();
+        public string TimeProp
+        {
+            get { return _timeProp; }
+            set { _timeProp = value; OnPropertyChanged("TimeProp"); }
+        }
+        private string _xbtimeProp = DateTime.Now.ToString();
+        public string XBTimeProp
+        {
+            get { return _xbtimeProp; }
+            set { _xbtimeProp = value; Bindings.Update(); }
+        }
+        private ObservableCollection<Model.Model> _modelItems;
+        public ObservableCollection<Model.Model> ModelItems
+        {
+            get
+            {
+                if (_modelItems == null) _modelItems = new ObservableCollection<Model.Model>();
+                return _modelItems;
+            }
+        }
+        private ObservableCollection<Model.XBModel> _xbModelItems;
+        public ObservableCollection<Model.XBModel> XBModelItems
+        {
+            get
+            {
+                if (_xbModelItems == null) _xbModelItems = new ObservableCollection<Model.XBModel>();
+                return _xbModelItems;
+            }
+        }
+
+
+        private DispatcherTimer dt = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += ((sender, e) => { TimeProp = XBTimeProp = DateTime.Now.ToString(); });
+            dt.Start();
+            ModelItems.Add(new Model.Model() { Name = "Martin", SurName = "Vega" });
+            XBModelItems.Add(new Model.XBModel() { Name = "Samuel", SurName = "Vega" });
+            Bindings.Update();
+            Model.XBModel.UpdateBindings += UpdateBindings;
+        }
+
+        private void UpdateBindings(object sender, EventArgs e)
+        {
+            Bindings.Update();
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ PoC/XBind/XBind/Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace XBind.Model
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged("Name"); } }
+        private string _surName;
+        public string SurName { get { return _surName; } set { _surName = value; OnPropertyChanged("SurName"); } }
+        private int _age;
+        public int Age { get { return _age; } set { _age = value; OnPropertyChanged("Age"); } }
+        public ICommand AgeIncrease { get { return new RelayCommand(() => { Age++; }); } }
+        public Model() { }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+    public class XBModel
+    {
+        public static event EventHandler UpdateBindings;
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+        private string _surName;
+        public string SurName { get { return _surName; } set { _surName = value; } }
+        private int _age;
+        public int Age { get { return _age; } set { _age = value; } }
+        public ICommand AgeIncrease { get { return new RelayCommand(() => { Age++; OnPropertyChanged(); }); } }
+
+        private void OnPropertyChanged()
+        {
+            if (UpdateBindings != null) UpdateBindings(null, EventArgs.Empty);
+        }
+
+        public XBModel() { }
+    }
+}
