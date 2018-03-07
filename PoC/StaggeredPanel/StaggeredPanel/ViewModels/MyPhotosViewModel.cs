f8527bab56cb17107a7bb5d10a64b8b1b399ce94
Martin Vega <mvegaca@outlook.com>
2018-03-07 18:10:58 +0100

    Add StaggeredPanel PoC

---
+++ PoC/StaggeredPanel/StaggeredPanel/ViewModels/MyPhotosViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using StaggeredPanel.Helpers;
+
+namespace StaggeredPanel.ViewModels
+{
+    public class MyPhotosViewModel : Observable
+    {
+        public ObservableCollection<string> Images { get; } = new ObservableCollection<string>();
+
+        public MyPhotosViewModel()
+        {
+        }
+
+        public void Initialize()
+        {
+            Images.Clear();
+            var rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                Images.Add($"https://picsum.photos/{rnd.Next(200, 500)}/{rnd.Next(200, 500)}");
+            }
+        }
+    }
+}
---
+++ PoC/StaggeredPanel/StaggeredPanel/Views/MyPhotosPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using StaggeredPanel.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace StaggeredPanel.Views
+{
+    public sealed partial class MyPhotosPage : Page
+    {
+        public MyPhotosViewModel ViewModel { get; } = new MyPhotosViewModel();
+
+        public MyPhotosPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            ViewModel.Initialize();
+        }
+    }
+}
